3d6316b8f61e9cfbd3d354d639466b09e031d98b
Jonathan Soma <jonathan.soma@gmail.com>
2016-08-07 09:44:33 -0400

    starting with capistrano

---
+++ playfair-update.rb
@@ -0,0 +1,24 @@
+require './playfair_bot'
+require 'pry'
+require 'slack'
+require 'optparse'
+
+options = { slack: false, github: false }
+OptionParser.new do |opts|
+  opts.banner = "Usage: playfair-update --github --slack"
+
+  opts.on("--slack", "Notify the Slack channel") do |v|
+    options[:slack] = true
+  end
+
+  opts.on("--github", "Update the GitHub repo") do |v|
+    options[:github] = true
+  end
+end.parse!
+
+bot = PlayfairBot::Bot.new()
+
+bot.process_issues if options[:github]
+bot.notify_slack if options[:slack]
+
+# binding.pry
